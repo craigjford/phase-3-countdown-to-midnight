0a4f15773f472b294e67003662c19475b491e98f
Craig Ford <craig_ford@icloud.com>
2022-07-26 17:45:07 -0400

    commit tree 36dfbec73cdc9e32a39cafdb19c39726923e7944

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(time_amt)
+  number = time_amt  
+  while number > 0   
+    puts "#{number} SECOND(S)"  
+    number -= 1
+    sleep 1
+  end
+  "HAPPY NEW YEAR!"
 end
+
+str = countdown(5)
+puts str
+
+
+
